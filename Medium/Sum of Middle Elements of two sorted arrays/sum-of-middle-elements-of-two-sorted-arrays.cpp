cc2b8efe4a5b051c3adae86ab90dc2df923b30d2
Aman Raj <90236749+amanraj5@users.noreply.github.com>
2023-04-18 13:19:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Middle Elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    int findMidSum(int a1[], int a2[], int n) {
+            // code here 
+            vector<int> ans;
+            for(int i=0;i<n;i++){
+                ans.push_back(a1[i]);
+            }
+            for(int i=0;i<n;i++){
+                ans.push_back(a2[i]);
+            }
+            sort(ans.begin(),ans.end());
+            return ans[n-1]+ans[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int ar1[n], ar2[n];
+        for (i = 0; i < n; i++) {
+            cin >> ar1[i];
+        }
+        for (i = 0; i < n; i++) {
+            cin >> ar2[i];
+        }
+        Solution ob;
+        auto ans = ob.findMidSum(ar1, ar2, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
