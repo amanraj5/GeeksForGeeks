8dde6fa04c4813abc3887eb1b02b4b584235a86d
Aman Raj <90236749+amanraj5@users.noreply.github.com>
2023-04-19 15:49:27 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximum no of 1's row/maximum-no-of-1s-row.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        int maxOnes (vector <vector <int>> &Mat, int N, int M)
+        {
+            // your code here
+            int ans=-1;
+            int i=0;
+            int j=M-1;
+            while(i<N && j>=0){
+                if(Mat[i][j]==1){
+                    j--;
+                    ans=i;
+                }
+                else{
+                    i++;
+                }
+                
+            }
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> arr[i][j];
+        Solution ob;        
+        cout << ob.maxOnes(arr, n, m) << endl;
+    }
+}
+// } Driver Code Ends
