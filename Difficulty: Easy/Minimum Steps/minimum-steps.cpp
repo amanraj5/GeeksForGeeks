ee17d42d66728a9979e0fa3f62ca58971e32a903
Aman Raj <90236749+amanraj5@users.noreply.github.com>
2024-09-21 13:14:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Steps/minimum-steps.cpp
+++ Difficulty: Easy/Minimum Steps/minimum-steps.cpp
@@ -12,14 +12,13 @@ class Solution {
         vector<int> dp(n+1,INT_MAX);
         dp[0]=0;
         for(int i=1;i<=n;i++){
-            if(i-p>=0 && dp[i-p]!=INT_MAX){
-                dp[i]=min(dp[i],dp[i-p]+1);
+            if(i>=p && dp[i-p]!=INT_MAX){
+                dp[i]=min(dp[i-p]+1,dp[i]);
             }
-            if(i-q>=0 && dp[i-q]!=INT_MAX){
-                dp[i]=min(dp[i],dp[i-q]+1);
+            if(i>=q && dp[i-q]!=INT_MAX){
+                dp[i]=min(dp[i-q]+1,dp[i]);
             }
         }
-        
         return dp[n]==INT_MAX?-1:dp[n];
     }
 };
